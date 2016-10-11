d41dd58ccf61c02c69098bc6787e0fc32c00b2d4
Frey-Fu@chinapex.com <frey.fu@chinapex.com>
2016-10-11 16:26:43 +0800

    Remove unused code in example.

--- example/src/app/app.component.ts
+++ example/src/app/app.component.ts
@@ -1,9 +1,7 @@
 /*
  * Angular 2 decorators and services
  */
-import { Component, ViewEncapsulation } from '@angular/core';
-
-import { AppState } from './app.service';
+import { Component } from '@angular/core';
 
 /*
  * App Component
@@ -11,76 +9,34 @@ import { AppState } from './app.service';
  */
 @Component({
   selector: 'app',
-  encapsulation: ViewEncapsulation.None,
   styleUrls: [
     './app.component.css'
   ],
   template: `
-    hello!
-    <nav>
-      <span>
-        <a [routerLink]=" ['./'] ">
-          Index
-        </a>
-      </span>
-      |
-      <span>
-        <a [routerLink]=" ['./home'] ">
-          Home
-        </a>
-      </span>
-      |
-      <span>
-        <a [routerLink]=" ['./detail'] ">
-          Detail
-        </a>
-      </span>
-      |
-      <span>
-        <a [routerLink]=" ['./about'] ">
-          About
-        </a>
-      </span>
-    </nav>
-    <main>
-      <router-outlet></router-outlet>
-    </main>
-    <pre class="app-state">this.appState.state = {{ appState.state | json }}</pre>
-    <footer>
-      <span>WebPack Angular 2 Starter by <a [href]="url">@AngularClass</a></span>
-      <div>
-        <a [href]="url">
-          <img [src]="angularclassLogo" width="25%">
-        </a>
-      </div>
-    </footer>
-    <div [ng2-echarts]="echarts30" class="graph" style="width:100%;height:100%;"></div>
+    Following is ECharts for NG2!
+  
+    <div [ng2-echarts]="echarts30" class="graph" style="width:800px;height:600px;"></div>
   `
 })
 export class App {
 
-  constructor(
-    public appState: AppState) {
-
-  }
-
   enabled_list_30: any[] = [
-        ['年轻爸爸',0.5, 80000],
-        ['健身一族',1.0, 10000],
+        ['Young father',0.5, 80000],
+        ['Young mother',1.0, 10000],
     ];
 
     disabled_list_30: any[] = [
-        ['豪华车关注者',0.8, 52000],
-        ['追星族',0.2, 53000],
-        ['电影达人',0.4, 70300]
+        ['Drivers',0.8, 52000],
+        ['JB Fans',0.2, 53000],
+        ['Readers',0.4, 70300]
     ];
 
     category_list: any[] = [
-        '年轻爸爸',
-        '健身一族',
-        '豪华车关注者',
-        '追星族',
-        '电影达人',
+        'Young father',
+        'Young mother',
+        'Drivers',
+        'JB Fans',
+        'Readers',
     ];
 
  echarts30 = {
@@ -112,7 +68,7 @@ export class App {
                 itemWidth: 15,
                 itemHeight: 50,
                 calculable: true,
-                text: ['人群增长率                    '], //a stupid trick to set the position of text
+                text: ['Increasing rate                    '], //a stupid trick to set the position of text
                 textGap: 30,
                 textStyle: {
                     color: '#000',
@@ -136,7 +92,7 @@ export class App {
         ],
         series: [
             {
-                name: '未激活',
+                name: 'Enabled',
                 type: 'scatter',
                 xAxisIndex: 0,
                 yAxisIndex: 0,
@@ -149,7 +105,7 @@ export class App {
                 }
             },
             {
-                name: '已激活',
+                name: 'Disabled',
                 type: 'scatter',
                 xAxisIndex: 0,
                 yAxisIndex: 0,
@@ -166,22 +122,13 @@ export class App {
             orient: 'vertical',
             y: '300px',
             x:'right',
-            data:[{name:'未激活'},{name: '已激活'}],
+            data:[{name:'Enabled'},{name: 'Disabled'}],
             textStyle: {
                 color: '#000'
             }
         },
     };
-  ngOnInit() {
-    console.log('Initial App State', this.appState.state);
-  }
 
 }
 
-/*
- * Please review the https://github.com/AngularClass/angular2-examples/ repo for
- * more angular app examples that you may copy/paste
- * (The examples may not be updated as quickly. Please open an issue on github for us to update it)
- * For help or questions please contact us at @AngularClass on twitter
- * or our chat on Slack at https://AngularClass.com/slack-join
- */
+
